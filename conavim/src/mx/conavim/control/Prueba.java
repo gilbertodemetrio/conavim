865b86de146326f1967f4569e03280d7aaae92a9
Gil <Gil@Gilberto>
2016-10-12 16:36:45 -0500

    Primera Version

---
+++ conavim/src/mx/conavim/control/Prueba.java
@@ -0,0 +1,16 @@
+package mx.conavim.control;
+
+public class Prueba {
+	private String mensaje="HOLA SOY UN BEAN";
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+	
+
+}
