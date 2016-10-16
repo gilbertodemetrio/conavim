0784b53c68cc96264e360f14fe5ffad3ba01e9c0
LuisEduardo <LuisEduardo@192.168.1.66>
2016-10-16 03:32:23 -0500

    se creo modelo para las respuestas

---
+++ conavim/src/mx/conavim/modelo/tblRespuestas.java
@@ -0,0 +1,266 @@
+package mx.conavim.modelo;
+
+public class tblRespuestas {
+	private String id_respuesta;
+	private String id_informe;
+	private String id_lineaaccion;
+	private String dependentsist;
+	private String activicumpla;
+	private String tipoactivi;
+	private String fechainactv;
+	private String fechatermactv;
+	private String producto;
+	private String tipoproduc;
+	private String linkproducto;
+	private String metaprgmdlinacc;
+	private String unidadmedlinacc;
+	private String periodometaprglinacc;
+	private String metaproglinacc;
+	private String metaprogactv;
+	private String periodometaprgactiv;
+	private String avance;
+	private String explicacionavance;
+	private String observaciones;
+	private String otrasinsticolaboran;
+	private String presupuesto;
+	private String fuentefinacia;
+	private String noserviciosotrg;
+	private String totalpoblbenfm;
+	private String poblabenfmujing;
+	private String totalpoblbenfh;
+	private String poblabenfhombing;
+	private String ninasbenifi0a12pobltot;
+	private String ninasbenifi0a12poblindig;
+	private String ninasbenifi12a17pobltot;
+	private String ninasbenifi12a17poblindig;
+	private String ninosbenifi0a12pobltot;
+	private String ninosbenifi0a12poblindig;
+	private String ninosbenifi12a17pobltot;
+	private String ninpsbenifi12a17poblindig;
+	private String status;
+	
+	
+	public String getId_respuesta() {
+		return id_respuesta;
+	}
+	public void setId_respuesta(String id_respuesta) {
+		this.id_respuesta = id_respuesta;
+	}
+	public String getId_informe() {
+		return id_informe;
+	}
+	public void setId_informe(String id_informe) {
+		this.id_informe = id_informe;
+	}
+	public String getId_lineaaccion() {
+		return id_lineaaccion;
+	}
+	public void setId_lineaaccion(String id_lineaaccion) {
+		this.id_lineaaccion = id_lineaaccion;
+	}
+	public String getDependentsist() {
+		return dependentsist;
+	}
+	public void setDependentsist(String dependentsist) {
+		this.dependentsist = dependentsist;
+	}
+	public String getActivicumpla() {
+		return activicumpla;
+	}
+	public void setActivicumpla(String activicumpla) {
+		this.activicumpla = activicumpla;
+	}
+	public String getTipoactivi() {
+		return tipoactivi;
+	}
+	public void setTipoactivi(String tipoactivi) {
+		this.tipoactivi = tipoactivi;
+	}
+	public String getFechainactv() {
+		return fechainactv;
+	}
+	public void setFechainactv(String fechainactv) {
+		this.fechainactv = fechainactv;
+	}
+	public String getFechatermactv() {
+		return fechatermactv;
+	}
+	public void setFechatermactv(String fechatermactv) {
+		this.fechatermactv = fechatermactv;
+	}
+	public String getProducto() {
+		return producto;
+	}
+	public void setProducto(String producto) {
+		this.producto = producto;
+	}
+	public String getTipoproduc() {
+		return tipoproduc;
+	}
+	public void setTipoproduc(String tipoproduc) {
+		this.tipoproduc = tipoproduc;
+	}
+	public String getLinkproducto() {
+		return linkproducto;
+	}
+	public void setLinkproducto(String linkproducto) {
+		this.linkproducto = linkproducto;
+	}
+	public String getMetaprgmdlinacc() {
+		return metaprgmdlinacc;
+	}
+	public void setMetaprgmdlinacc(String metaprgmdlinacc) {
+		this.metaprgmdlinacc = metaprgmdlinacc;
+	}
+	public String getUnidadmedlinacc() {
+		return unidadmedlinacc;
+	}
+	public void setUnidadmedlinacc(String unidadmedlinacc) {
+		this.unidadmedlinacc = unidadmedlinacc;
+	}
+	public String getPeriodometaprglinacc() {
+		return periodometaprglinacc;
+	}
+	public void setPeriodometaprglinacc(String periodometaprglinacc) {
+		this.periodometaprglinacc = periodometaprglinacc;
+	}
+	public String getMetaproglinacc() {
+		return metaproglinacc;
+	}
+	public void setMetaproglinacc(String metaproglinacc) {
+		this.metaproglinacc = metaproglinacc;
+	}
+	public String getMetaprogactv() {
+		return metaprogactv;
+	}
+	public void setMetaprogactv(String metaprogactv) {
+		this.metaprogactv = metaprogactv;
+	}
+	public String getPeriodometaprgactiv() {
+		return periodometaprgactiv;
+	}
+	public void setPeriodometaprgactiv(String periodometaprgactiv) {
+		this.periodometaprgactiv = periodometaprgactiv;
+	}
+	public String getAvance() {
+		return avance;
+	}
+	public void setAvance(String avance) {
+		this.avance = avance;
+	}
+	public String getExplicacionavance() {
+		return explicacionavance;
+	}
+	public void setExplicacionavance(String explicacionavance) {
+		this.explicacionavance = explicacionavance;
+	}
+	public String getObservaciones() {
+		return observaciones;
+	}
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+	public String getOtrasinsticolaboran() {
+		return otrasinsticolaboran;
+	}
+	public void setOtrasinsticolaboran(String otrasinsticolaboran) {
+		this.otrasinsticolaboran = otrasinsticolaboran;
+	}
+	public String getPresupuesto() {
+		return presupuesto;
+	}
+	public void setPresupuesto(String presupuesto) {
+		this.presupuesto = presupuesto;
+	}
+	public String getFuentefinacia() {
+		return fuentefinacia;
+	}
+	public void setFuentefinacia(String fuentefinacia) {
+		this.fuentefinacia = fuentefinacia;
+	}
+	public String getNoserviciosotrg() {
+		return noserviciosotrg;
+	}
+	public void setNoserviciosotrg(String noserviciosotrg) {
+		this.noserviciosotrg = noserviciosotrg;
+	}
+	public String getTotalpoblbenfm() {
+		return totalpoblbenfm;
+	}
+	public void setTotalpoblbenfm(String totalpoblbenfm) {
+		this.totalpoblbenfm = totalpoblbenfm;
+	}
+	public String getPoblabenfmujing() {
+		return poblabenfmujing;
+	}
+	public void setPoblabenfmujing(String poblabenfmujing) {
+		this.poblabenfmujing = poblabenfmujing;
+	}
+	public String getTotalpoblbenfh() {
+		return totalpoblbenfh;
+	}
+	public void setTotalpoblbenfh(String totalpoblbenfh) {
+		this.totalpoblbenfh = totalpoblbenfh;
+	}
+	public String getPoblabenfhombing() {
+		return poblabenfhombing;
+	}
+	public void setPoblabenfhombing(String poblabenfhombing) {
+		this.poblabenfhombing = poblabenfhombing;
+	}
+	public String getNinasbenifi0a12pobltot() {
+		return ninasbenifi0a12pobltot;
+	}
+	public void setNinasbenifi0a12pobltot(String ninasbenifi0a12pobltot) {
+		this.ninasbenifi0a12pobltot = ninasbenifi0a12pobltot;
+	}
+	public String getNinasbenifi0a12poblindig() {
+		return ninasbenifi0a12poblindig;
+	}
+	public void setNinasbenifi0a12poblindig(String ninasbenifi0a12poblindig) {
+		this.ninasbenifi0a12poblindig = ninasbenifi0a12poblindig;
+	}
+	public String getNinasbenifi12a17pobltot() {
+		return ninasbenifi12a17pobltot;
+	}
+	public void setNinasbenifi12a17pobltot(String ninasbenifi12a17pobltot) {
+		this.ninasbenifi12a17pobltot = ninasbenifi12a17pobltot;
+	}
+	public String getNinasbenifi12a17poblindig() {
+		return ninasbenifi12a17poblindig;
+	}
+	public void setNinasbenifi12a17poblindig(String ninasbenifi12a17poblindig) {
+		this.ninasbenifi12a17poblindig = ninasbenifi12a17poblindig;
+	}
+	public String getNinosbenifi0a12pobltot() {
+		return ninosbenifi0a12pobltot;
+	}
+	public void setNinosbenifi0a12pobltot(String ninosbenifi0a12pobltot) {
+		this.ninosbenifi0a12pobltot = ninosbenifi0a12pobltot;
+	}
+	public String getNinosbenifi0a12poblindig() {
+		return ninosbenifi0a12poblindig;
+	}
+	public void setNinosbenifi0a12poblindig(String ninosbenifi0a12poblindig) {
+		this.ninosbenifi0a12poblindig = ninosbenifi0a12poblindig;
+	}
+	public String getNinosbenifi12a17pobltot() {
+		return ninosbenifi12a17pobltot;
+	}
+	public void setNinosbenifi12a17pobltot(String ninosbenifi12a17pobltot) {
+		this.ninosbenifi12a17pobltot = ninosbenifi12a17pobltot;
+	}
+	public String getNinpsbenifi12a17poblindig() {
+		return ninpsbenifi12a17poblindig;
+	}
+	public void setNinpsbenifi12a17poblindig(String ninpsbenifi12a17poblindig) {
+		this.ninpsbenifi12a17poblindig = ninpsbenifi12a17poblindig;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
