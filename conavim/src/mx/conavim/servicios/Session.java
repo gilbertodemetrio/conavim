f09c9a1ce85bedf95d37743e6a6dfa1b00f4c53f
LuisEduardo <LuisEduardo@192.168.1.66>
2016-10-16 03:12:17 -0500

    se cre la clase para cerrar la sesion

---
+++ conavim/src/mx/conavim/servicios/Session.java
@@ -0,0 +1,37 @@
+package mx.conavim.servicios;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+public class Session {
+	
+	public static void close()
+	{
+		try
+	  	{
+			System.out.println("cerro sesion");
+			removeBeans();
+			getSession().invalidate();
+			FacesContext.getCurrentInstance()
+			   .getExternalContext().redirect("secure/");
+	  	}catch(Exception ex)
+	  	{
+	  		ex.printStackTrace();
+	  	}
+	}
+	
+	private static HttpSession getSession() {
+        return (HttpSession)
+          FacesContext.
+          getCurrentInstance().
+          getExternalContext().
+          getSession(false);
+      }
+	
+	private static void removeBeans() throws Exception
+	{
+		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove("prueba");
+		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove("mbTbl_LineasAccion");
+	}
+
+}
