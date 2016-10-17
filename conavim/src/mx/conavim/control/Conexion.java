8597ec81502c62e1bdfe4eb28ba5f01db50d1c64
redalyc <redalyc@David-THINK>
2016-10-16 20:04:50 -0500

    Se cambio a clase abstracta

--- conavim/src/mx/conavim/control/Conexion.java
+++ conavim/src/mx/conavim/control/Conexion.java
@@ -2,60 +2,64 @@ package mx.conavim.control;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 
 
 
 
-public class Conexion {
+public abstract class Conexion {
 	
 	   static String bd = "conavim";
+	   //parametros para base local
+	   
 	   static String user = "postgres";
-	   //static String user = "postgres";
-	   static String password = "ardillatiko";
-	   //static String password = "";
-	   //static String url = "jdbc:postgresql://node21778-conavim.jl.serv.net.mx/"+bd;
+	   static String password = "admin";
 	   static String url = "jdbc:postgresql://localhost:5432/"+bd;
 	   
-	   Connection connection = null;
+	   //parametros para base de jelastic
 	   
-	   public Conexion(){
-		   try{
-		         //obtenemos el driver de para postgresql
-		         Class.forName("org.postgresql.Driver");
-		         //obtenemos la conexión
-		         //String URL =  "jdbc:postgresql://node21771-conavimprueba.jl.serv.net.mx/{your_database_name}";
-		         //DriverManager.getConnection(URL, user_name, user_password);
-		         connection = DriverManager.getConnection(url,user,password);
-		 
-		         if (connection!=null){
-		            //System.out.println("Conexión a base de datos "+bd+" OK\n");
-		         }
-		      }
-		      catch(SQLException e){
-		         System.out.println(e);
-		      }catch(ClassNotFoundException e){
-		         System.out.println(e);
-		      }catch(Exception e){
-		         System.out.println(e);
-		      }
+	   
+	   //static String password = "SNHxas17182";
+	   //static String url = "jdbc:postgresql://node21901-env-7716639.jl.serv.net.mx/"+bd;
+	 //static String user = "webadmin";
+	   
+	   
+	   
+	   private Connection connection = null;
+	   
+	   protected ResultSet res = null;
+	   protected PreparedStatement consulta = null;
+		   
+	   public Connection getConnection() {
+		   return connection;
 	   }
-	   
-	   /**Permite retornar la conexión*/
-	   public Connection conectar(){
-	      return connection;
+
+	public Conexion()
+	   {
+		   try
+		   {
+			   Class.forName("org.postgresql.Driver");
+		       connection = DriverManager.getConnection(url, user, password);
+		   }
+		   catch(Exception ex)
+		   {
+			   ex.printStackTrace();
+		   }
 	   }
 	 
-	   public void desconectar(){
-	      
-	      try {
-	    	//connection = null;
-			conectar().close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	   public void desconectar()
+	   {
+	       try 
+	       {
+	    	   if(!connection.isClosed())
+	    		   getConnection().close();
+		   } 
+	       catch (Exception ex) 
+	       {
+	    	   ex.printStackTrace();
+		   }
 	   }	   
-}
-	
+	}
+		
